2bb5f6d671418a274a94cc62fc41cf7053a0fd6c
JoachimR <joachim.renard@student.Vinci.be>
2020-11-01 12:31:31 +0100

    add - implementSingleMin for input 42

--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -15,6 +15,14 @@ class BerlinClockTest extends TestCase
 
     }
 
+    public function test_convertToSingleMinutes_given42Minutes_shouldReturn_array_yyyo(){
+        $berlinClock = new BerlinClock();
+        $actual = $berlinClock->implementSingleMin(42);
+        $tabExpected = array('y','y','o','o');
+        $this->assertEquals($tabExpected,$actual);
+
+    }
+
     public function test_convertToBlockOfFiveMinutes_given53Minutes_shouldReturn_array_yyryyryyryo(){
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementBlockOfFiveMin(53);
@@ -22,6 +30,8 @@ class BerlinClockTest extends TestCase
         $this->assertEquals($tabExpected,$actual);
 
     }
+
+
 
 
 
