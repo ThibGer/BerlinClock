8dbb0249eb379cda9e2d3bd525bd2742878f50c0
JoachimR <joachim.renard@student.Vinci.be>
2020-11-03 16:58:52 +0100

    indentation

--- BerlinClock.php
+++ BerlinClock.php
@@ -4,10 +4,10 @@ class BerlinClock
 {
     public function implementSingleMin(int $min): array
     {
-        if ($min<0 || $min>59)
+        if ($min < 0 || $min > 59)
             throw new Exception("$min is not available");
-        $tab = array('o','o','o','o');
-        for($i=0;$i<$min%5;$i++){
+        $tab = array('o', 'o', 'o', 'o');
+        for ($i = 0; $i < $min % 5; $i++) {
             $tab[$i] = 'y';
         }
         return $tab;
@@ -15,24 +15,24 @@ class BerlinClock
 
     public function implementBlockOfFiveMin(int $min): array
     {
-        if ($min<0 || $min>59)
+        if ($min < 0 || $min > 59)
             throw new Exception("$min is not available");
-        $tab = array('o','o','o','o','o','o','o','o','o','o','o');
-        for($i=1;$i<=$min/5;$i++){
-            if ($i%3==0)
-                $tab[$i-1] = 'r';
+        $tab = array('o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o');
+        for ($i = 1; $i <= $min / 5; $i++) {
+            if ($i % 3 == 0)
+                $tab[$i - 1] = 'r';
             else
-                $tab[$i-1] = 'y';
+                $tab[$i - 1] = 'y';
         }
         return $tab;
     }
 
     public function implementSingleHours(int $hour): array
     {
-        if ($hour<0 || $hour>23)
+        if ($hour < 0 || $hour > 23)
             throw new Exception("$hour is not available");
-        $tab = array('o','o','o','o');
-        for($i=0;$i<$hour%5;$i++){
+        $tab = array('o', 'o', 'o', 'o');
+        for ($i = 0; $i < $hour % 5; $i++) {
             $tab[$i] = 'r';
         }
         return $tab;
@@ -40,31 +40,31 @@ class BerlinClock
 
     public function implementBlockOfFiveHours(int $hour): array
     {
-        if ($hour<0 || $hour>23)
+        if ($hour < 0 || $hour > 23)
             throw new Exception("$hour is not available");
-        $tab = array('o','o','o','o');
-        for($i=1;$i<=$hour/5;$i++){
-                $tab[$i-1] = 'r';
+        $tab = array('o', 'o', 'o', 'o');
+        for ($i = 1; $i <= $hour / 5; $i++) {
+            $tab[$i - 1] = 'r';
         }
         return $tab;
     }
 
     public function implementSeconds(int $sec): string
     {
-        if($sec<0 || $sec>59){
+        if ($sec < 0 || $sec > 59) {
             throw new Exception("$sec is not available");
         }
-        if ($sec%2==0)
+        if ($sec % 2 == 0)
             return 'r';
         return 'o';
     }
 
     public function implementClock(string $time): array
     {
-        if(preg_match("/^[0-9]{2}:[0-9]{2}:[0-9]{2}$/", $time) != 1)
+        if (preg_match("/^[0-9]{2}:[0-9]{2}:[0-9]{2}$/", $time) != 1)
             throw new Exception("$time is not available format");
 
-        $min = intval(substr($time,3,2));
+        $min = intval(substr($time, 3, 2));
         $hour = intval(substr($time, 0, 2));
         $sec = intval(substr($time, 6, 2));
 
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -2,312 +2,329 @@
 
 //require "vendor/autoload.php";
 require "BerlinClock.php";
+
 use PHPUnit\Framework\TestCase;
 
 class BerlinClockTest extends TestCase
 {
 
-    public function test_convertToSingleMinutes_given53Minutes_shouldReturn_array_yyyo(){
+    public function test_convertToSingleMinutes_given53Minutes_shouldReturn_array_yyyo()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementSingleMin(53);
-        $tabExpected = array('y','y','y','o');
-        $this->assertEquals($tabExpected,$actual);
+        $tabExpected = array('y', 'y', 'y', 'o');
+        $this->assertEquals($tabExpected, $actual);
 
     }
 
-    public function test_convertToSingleMinutes_given42Minutes_shouldReturn_array_yyoo(){
+    public function test_convertToSingleMinutes_given42Minutes_shouldReturn_array_yyoo()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementSingleMin(42);
-        $tabExpected = array('y','y','o','o');
-        $this->assertEquals($tabExpected,$actual);
+        $tabExpected = array('y', 'y', 'o', 'o');
+        $this->assertEquals($tabExpected, $actual);
 
     }
 
-    public function test_convertToSingleMinutes_given87Minutes_shouldReturn_exception(){
+    public function test_convertToSingleMinutes_given87Minutes_shouldReturn_exception()
+    {
         $berlinClock = new BerlinClock();
         $min = 87;
-        try{
+        try {
             $actual = $berlinClock->implementSingleMin($min);
-        } catch(Exception $e){
-            $this->assertEquals("$min is not available",$e->getMessage());
+        } catch (Exception $e) {
+            $this->assertEquals("$min is not available", $e->getMessage());
         }
 
     }
 
-    public function test_convertToBlockOfFiveMinutes_given53Minutes_shouldReturn_array_yyryyryyryo(){
+    public function test_convertToBlockOfFiveMinutes_given53Minutes_shouldReturn_array_yyryyryyryo()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementBlockOfFiveMin(53);
-        $tabExpected = array('y','y','r', 'y', 'y', 'r', 'y', 'y', 'r', 'y','o');
-        $this->assertEquals($tabExpected,$actual);
+        $tabExpected = array('y', 'y', 'r', 'y', 'y', 'r', 'y', 'y', 'r', 'y', 'o');
+        $this->assertEquals($tabExpected, $actual);
 
     }
 
-    public function test_convertToBlockOfFiveMinutes_given24Minutes_shouldReturn_array_yyryooooooo(){
+    public function test_convertToBlockOfFiveMinutes_given24Minutes_shouldReturn_array_yyryooooooo()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementBlockOfFiveMin(24);
-        $tabExpected = array('y','y','r', 'y', 'o', 'o', 'o', 'o', 'o', 'o','o');
-        $this->assertEquals($tabExpected,$actual);
+        $tabExpected = array('y', 'y', 'r', 'y', 'o', 'o', 'o', 'o', 'o', 'o', 'o');
+        $this->assertEquals($tabExpected, $actual);
 
     }
 
-    public function test_convertToBlockOfFiveMinutes_given3NegativeNumberMinutes_shouldReturn_exception(){
+    public function test_convertToBlockOfFiveMinutes_given3NegativeNumberMinutes_shouldReturn_exception()
+    {
         $berlinClock = new BerlinClock();
         $min = -3;
-        try{
+        try {
             $actual = $berlinClock->implementBlockOfFiveMin($min);
-        } catch(Exception $e){
-            $this->assertEquals("$min is not available",$e->getMessage());
+        } catch (Exception $e) {
+            $this->assertEquals("$min is not available", $e->getMessage());
         }
 
     }
 
-    public function test_convertToSingleHours_given12Hours_shouldReturn_array_rroo(){
+    public function test_convertToSingleHours_given12Hours_shouldReturn_array_rroo()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementSingleHours(12);
-        $tabExpected = array('r','r','o','o');
-        $this->assertEquals($tabExpected,$actual);
+        $tabExpected = array('r', 'r', 'o', 'o');
+        $this->assertEquals($tabExpected, $actual);
 
     }
 
-    public function test_convertToSingleHours_given1Hour_shouldReturn_array_rooo(){
+    public function test_convertToSingleHours_given1Hour_shouldReturn_array_rooo()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementSingleHours(1);
-        $tabExpected = array('r','o','o','o');
-        $this->assertEquals($tabExpected,$actual);
+        $tabExpected = array('r', 'o', 'o', 'o');
+        $this->assertEquals($tabExpected, $actual);
 
     }
 
-    public function test_convertToSingleHours_given0Hour_shouldReturn_array_oooo(){
+    public function test_convertToSingleHours_given0Hour_shouldReturn_array_oooo()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementSingleHours(0);
-        $tabExpected = array('o','o','o','o');
-        $this->assertEquals($tabExpected,$actual);
+        $tabExpected = array('o', 'o', 'o', 'o');
+        $this->assertEquals($tabExpected, $actual);
 
     }
 
-    public function test_convertToSingleHours_given24Hours_shouldReturn_exception(){
+    public function test_convertToSingleHours_given24Hours_shouldReturn_exception()
+    {
         $berlinClock = new BerlinClock();
         $hour = 24;
-        try{
+        try {
             $actual = $berlinClock->implementSingleHours($hour);
-        } catch(Exception $e){
-            $this->assertEquals("$hour is not available",$e->getMessage());
+        } catch (Exception $e) {
+            $this->assertEquals("$hour is not available", $e->getMessage());
         }
 
     }
 
-    public function test_convertToBlockOfFiveHours_given12Hours_shouldReturn_array_rroo(){
+    public function test_convertToBlockOfFiveHours_given12Hours_shouldReturn_array_rroo()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementBlockOfFiveHours(12);
-        $tabExpected = array('r','r','o','o');
-        $this->assertEquals($tabExpected,$actual);
+        $tabExpected = array('r', 'r', 'o', 'o');
+        $this->assertEquals($tabExpected, $actual);
 
     }
 
-    public function test_convertToBlockOfFiveHours_given23Hours_shouldReturn_array_rrrr(){
+    public function test_convertToBlockOfFiveHours_given23Hours_shouldReturn_array_rrrr()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementBlockOfFiveHours(23);
-        $tabExpected = array('r','r','r','r');
-        $this->assertEquals($tabExpected,$actual);
+        $tabExpected = array('r', 'r', 'r', 'r');
+        $this->assertEquals($tabExpected, $actual);
 
     }
 
-    public function test_convertToBlockOfFiveHours_given2Hours_shouldReturn_array_oooo(){
+    public function test_convertToBlockOfFiveHours_given2Hours_shouldReturn_array_oooo()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementBlockOfFiveHours(2);
-        $tabExpected = array('o','o','o','o');
-        $this->assertEquals($tabExpected,$actual);
+        $tabExpected = array('o', 'o', 'o', 'o');
+        $this->assertEquals($tabExpected, $actual);
 
     }
 
-    public function test_convertToBlockOfFiveHours_given5NegativeNumberHours_shouldReturn_exception(){
+    public function test_convertToBlockOfFiveHours_given5NegativeNumberHours_shouldReturn_exception()
+    {
         $berlinClock = new BerlinClock();
         $hour = -5;
-        try{
+        try {
             $actual = $berlinClock->implementBlockOfFiveHours($hour);
-        } catch(Exception $e){
-            $this->assertEquals("$hour is not available",$e->getMessage());
+        } catch (Exception $e) {
+            $this->assertEquals("$hour is not available", $e->getMessage());
         }
 
     }
 
-    public function test_convertToSeconds_given50Seconds_shouldReturn_string_r(){
+    public function test_convertToSeconds_given50Seconds_shouldReturn_string_r()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementSeconds(50);
-        $this->assertEquals('r',$actual);
+        $this->assertEquals('r', $actual);
 
     }
 
-    public function test_convertToSeconds_given1Second_shouldReturn_string_o(){
+    public function test_convertToSeconds_given1Second_shouldReturn_string_o()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementSeconds(1);
-        $this->assertEquals('o',$actual);
+        $this->assertEquals('o', $actual);
 
     }
 
-    public function test_convertToSeconds_given0Second_shouldReturn_string_r(){
+    public function test_convertToSeconds_given0Second_shouldReturn_string_r()
+    {
         $berlinClock = new BerlinClock();
         $actual = $berlinClock->implementSeconds(0);
-        $this->assertEquals('r',$actual);
+        $this->assertEquals('r', $actual);
 
     }
 
-    public function test_convertToSeconds_given60Seconds_shouldReturn_exception(){
+    public function test_convertToSeconds_given60Seconds_shouldReturn_exception()
+    {
         $berlinClock = new BerlinClock();
         $sec = 60;
-        try{
+        try {
             $actual = $berlinClock->implementSeconds($sec);
-        } catch(Exception $e){
-            $this->assertEquals("$sec is not available",$e->getMessage());
+        } catch (Exception $e) {
+            $this->assertEquals("$sec is not available", $e->getMessage());
         }
 
     }
 
-    public function test_convertToSeconds_given200NegativeNumberSeconds_shouldReturn_exception(){
+    public function test_convertToSeconds_given200NegativeNumberSeconds_shouldReturn_exception()
+    {
         $berlinClock = new BerlinClock();
         $sec = -200;
-        try{
+        try {
             $actual = $berlinClock->implementSeconds($sec);
-        } catch(Exception $e){
-            $this->assertEquals("$sec is not available",$e->getMessage());
+        } catch (Exception $e) {
+            $this->assertEquals("$sec is not available", $e->getMessage());
         }
 
     }
 
-    public function test_convertToBerlinClock_given11H56M45S_shouldReturn_arrayOfArray_o_rroo_rooo_yyryyryyryy_yooo(){
+    public function test_convertToBerlinClock_given11H56M45S_shouldReturn_arrayOfArray_o_rroo_rooo_yyryyryyryy_yooo()
+    {
         $berlinClock = new BerlinClock();
         $time = "11:56:45";
 
         $tabSec = array('o');
-        $tabBlock5Hours = array('r','r','o','o');
-        $tabSingleHours = array('r','o','o','o');
-        $tabBlock5Min = array('y','y','r','y','y','r','y','y','r','y','y');
-        $tabSingleMin = array('y','o','o','o');
+        $tabBlock5Hours = array('r', 'r', 'o', 'o');
+        $tabSingleHours = array('r', 'o', 'o', 'o');
+        $tabBlock5Min = array('y', 'y', 'r', 'y', 'y', 'r', 'y', 'y', 'r', 'y', 'y');
+        $tabSingleMin = array('y', 'o', 'o', 'o');
 
         $clock = array($tabSec, $tabBlock5Hours, $tabSingleHours, $tabBlock5Min, $tabSingleMin);
 
-        try{
+        try {
             $actual = $berlinClock->implementClock($time);
-            $this->assertEquals($clock,$actual);
-        } catch(Exception $e){
-            $this->assertEquals("$time is not available format",$e->getMessage());
+            $this->assertEquals($clock, $actual);
+        } catch (Exception $e) {
+            $this->assertEquals("$time is not available format", $e->getMessage());
         }
 
     }
 
-    public function test_convertToBerlinClock_given23H07M02S_shouldReturn_arrayOfArray_r_rrrr_rrro_yoooooooooo_yyoo(){
+    public function test_convertToBerlinClock_given23H07M02S_shouldReturn_arrayOfArray_r_rrrr_rrro_yoooooooooo_yyoo()
+    {
         $berlinClock = new BerlinClock();
         $time = "23:07:02";
 
         $tabSec = array('r');
-        $tabBlock5Hours = array('r','r','r','r');
-        $tabSingleHours = array('r','r','r','o');
-        $tabBlock5Min = array('y','o','o','o','o','o','o','o','o','o','o');
-        $tabSingleMin = array('y','y','o','o');
+        $tabBlock5Hours = array('r', 'r', 'r', 'r');
+        $tabSingleHours = array('r', 'r', 'r', 'o');
+        $tabBlock5Min = array('y', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o');
+        $tabSingleMin = array('y', 'y', 'o', 'o');
 
         $clock = array($tabSec, $tabBlock5Hours, $tabSingleHours, $tabBlock5Min, $tabSingleMin);
 
-        try{
+        try {
             $actual = $berlinClock->implementClock($time);
-            $this->assertEquals($clock,$actual);
-        } catch(Exception $e){
-            $this->assertEquals("$time is not available format",$e->getMessage());
+            $this->assertEquals($clock, $actual);
+        } catch (Exception $e) {
+            $this->assertEquals("$time is not available format", $e->getMessage());
         }
 
     }
 
-    public function test_convertToBerlinClock_givenWrongFormatTime_shouldReturn_exception(){
+    public function test_convertToBerlinClock_givenWrongFormatTime_shouldReturn_exception()
+    {
         $berlinClock = new BerlinClock();
         $time = "23H07M02";
 
         $tabSec = array('r');
-        $tabBlock5Hours = array('r','r','r','r');
-        $tabSingleHours = array('r','r','r','o');
-        $tabBlock5Min = array('y','o','o','o','o','o','o','o','o','o','o');
-        $tabSingleMin = array('y','y','o','o');
+        $tabBlock5Hours = array('r', 'r', 'r', 'r');
+        $tabSingleHours = array('r', 'r', 'r', 'o');
+        $tabBlock5Min = array('y', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o');
+        $tabSingleMin = array('y', 'y', 'o', 'o');
 
         $clock = array($tabSec, $tabBlock5Hours, $tabSingleHours, $tabBlock5Min, $tabSingleMin);
 
-        try{
+        try {
             $actual = $berlinClock->implementClock($time);
-            $this->assertEquals($clock,$actual);
-        } catch(Exception $e){
-            $this->assertEquals("$time is not available format",$e->getMessage());
+            $this->assertEquals($clock, $actual);
+        } catch (Exception $e) {
+            $this->assertEquals("$time is not available format", $e->getMessage());
         }
 
     }
 
-    public function test_convertToBerlinClock_givenWrongFormatTime_secondVersion_shouldReturn_exception(){
+    public function test_convertToBerlinClock_givenWrongFormatTime_secondVersion_shouldReturn_exception()
+    {
         $berlinClock = new BerlinClock();
         $time = "23:7:2";
 
         $tabSec = array('r');
-        $tabBlock5Hours = array('r','r','r','r');
-        $tabSingleHours = array('r','r','r','o');
-        $tabBlock5Min = array('y','o','o','o','o','o','o','o','o','o','o');
-        $tabSingleMin = array('y','y','o','o');
+        $tabBlock5Hours = array('r', 'r', 'r', 'r');
+        $tabSingleHours = array('r', 'r', 'r', 'o');
+        $tabBlock5Min = array('y', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o');
+        $tabSingleMin = array('y', 'y', 'o', 'o');
 
         $clock = array($tabSec, $tabBlock5Hours, $tabSingleHours, $tabBlock5Min, $tabSingleMin);
 
-        try{
+        try {
             $actual = $berlinClock->implementClock($time);
-            $this->assertEquals($clock,$actual);
-        } catch(Exception $e){
-            $this->assertEquals("$time is not available format",$e->getMessage());
+            $this->assertEquals($clock, $actual);
+        } catch (Exception $e) {
+            $this->assertEquals("$time is not available format", $e->getMessage());
         }
 
     }
 
-    public function test_convertToBerlinClock_given98H07M02S_shouldReturn_exception(){
+    public function test_convertToBerlinClock_given98H07M02S_shouldReturn_exception()
+    {
         $berlinClock = new BerlinClock();
         $time = "98:07:02";
 
         $tabSec = array('r');
-        $tabBlock5Hours = array('r','r','r','r');
-        $tabSingleHours = array('r','r','r','r');
-        $tabBlock5Min = array('y','o','o','o','o','o','o','o','o','o','o');
-        $tabSingleMin = array('y','y','o','o');
+        $tabBlock5Hours = array('r', 'r', 'r', 'r');
+        $tabSingleHours = array('r', 'r', 'r', 'r');
+        $tabBlock5Min = array('y', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o');
+        $tabSingleMin = array('y', 'y', 'o', 'o');
 
         $clock = array($tabSec, $tabBlock5Hours, $tabSingleHours, $tabBlock5Min, $tabSingleMin);
 
-        try{
+        try {
             $actual = $berlinClock->implementClock($time);
-            $this->assertEquals($clock,$actual);
-        } catch(Exception $e){
-            $this->assertEquals("98 is not available",$e->getMessage());
+            $this->assertEquals($clock, $actual);
+        } catch (Exception $e) {
+            $this->assertEquals("98 is not available", $e->getMessage());
         }
 
     }
 
-    public function test_convertToBerlinClock_given15H_07NegativeNumberM_02S_shouldReturn_exception(){
+    public function test_convertToBerlinClock_given15H_07NegativeNumberM_02S_shouldReturn_exception()
+    {
         $berlinClock = new BerlinClock();
         $time = "15:-7:02";
 
         $tabSec = array('r');
-        $tabBlock5Hours = array('r','r','r','o');
-        $tabSingleHours = array('o','o','o','o');
-        $tabBlock5Min = array('y','o','o','o','o','o','o','o','o','o','o');
-        $tabSingleMin = array('y','y','o','o');
+        $tabBlock5Hours = array('r', 'r', 'r', 'o');
+        $tabSingleHours = array('o', 'o', 'o', 'o');
+        $tabBlock5Min = array('y', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o');
+        $tabSingleMin = array('y', 'y', 'o', 'o');
 
         $clock = array($tabSec, $tabBlock5Hours, $tabSingleHours, $tabBlock5Min, $tabSingleMin);
 
-        try{
+        try {
             $actual = $berlinClock->implementClock($time);
-            $this->assertEquals($clock,$actual);
-        } catch(Exception $e){
-            $this->assertEquals("$time is not available format",$e->getMessage());
+            $this->assertEquals($clock, $actual);
+        } catch (Exception $e) {
+            $this->assertEquals("$time is not available format", $e->getMessage());
         }
 
     }
-
-
-
-
-
-
-
-
-
 
 
 }
